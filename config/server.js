06e54811941cbed8af1cc173353f1b4e7e281afc
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-03-17 02:55:33 +0100

    🚀 create-strapi-app

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f6d8ecc89205625adb52ff669b2aa1fd'),
+    },
+  },
+});
