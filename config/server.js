5bb1e725825ba3fcd46e3854ed30279139cdbe07
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-03-27 00:43:58 +0100

    Delete server.js

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '22befb2e3a60a3226173c56e3febb018'),
-    },
-  },
-});
